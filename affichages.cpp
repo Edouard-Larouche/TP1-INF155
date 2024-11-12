b8afeffaf559f5650b55a124c8b6bc9e33522ad6
wchung7290 <william4090@outlook.fr>
2024-11-12 17:32:44 -0500

    Update affichages.cpp

--- affichages.cpp
+++ affichages.cpp
@@ -26,8 +26,7 @@ FILE* lire_nom_fich() {
             printf("OUPS... Ca ne marche pas !\n");
         }
     }
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("Entree valide.\n");
+    
     return ptr_de_fichier;
 }
 
@@ -51,28 +50,6 @@ void colorage_plaque(const t_matrice plaque, int dimy, int dimx, double mint, do
     // Réinitialise la couleur de fond
     textbackground(0);
 
-    // Affichage de l'échelle des couleurs en bas à droite de l’écran
-    int ligne_echelle = MAXLIG - NB_COUL - 2;  // Position de l’échelle en bas
-    int col_echelle = COL_DROITE + 10;         // Position de l’échelle à droite
-    double temp_min, temp_max;
-    gotoxy(col_echelle, ligne_echelle);
-
-    for (i = 0; i < NB_COUL; i++) {
-        temp_min = mint + i * ecart;  // Température minimale de la couleur actuelle
-        temp_max = temp_min + ecart;  // Température maximale de la couleur actuelle
-
-        // Affiche la couleur
-        textbackground(COULEURS[i]);
-        printf("  ");  // Bloc de couleur
-        textbackground(0);  // Réinitialise la couleur de fond pour la légende
-
-        // Affiche la légende avec la plage de températures
-        printf(" %.2lf - %.2lf", temp_min, temp_max);
-        gotoxy(col_echelle, ligne_echelle + i + 1);  // Passe à la ligne suivante
-    }
-
-    textbackground(0); // Réinitialiser la couleur de fond après l’échelle
-
 }
 
 int simulation(t_matrice plaque, t_matbool pos_fixes, int dimy, int dimx,
@@ -91,113 +68,12 @@ int simulation(t_matrice plaque, t_matbool pos_fixes, int dimy, int dimx,
         moyenne = copier_nouv_plaque(plaque, nouv_plaque,
             dimy, dimx, mint, maxt);
         colorage_plaque(plaque, dimy, dimx, *mint, *maxt);
-        gotoxy(COL_DROITE, LINGNE_INFO_TABLE);
-        printf("Max : %.2lf | Min : %.2lf | Moyenne : %.2lf", *maxt, *mint, moyenne);
+        aff_minmax_moy(*mint, *maxt, moyenne); 
         iteration++;
     } while (equilibre != 1);
 
     return iteration;
 }
-
-unsigned char afficher_menu(double* epsilon, double* coeff, int* mode, int* init,
-    t_matrice plaque, t_matbool pos_fixes, int* dimy, int* dimx,
-    double* mint, double* maxt) {
-
-    int iteration_n;
-    unsigned char choix;
-    int ligne_ecriture = 0;  // Position du menu en bas de l'écran
-
-
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("Menu:\n");
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("1. (L)ire donnees initiales d'un fichier\n");
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("2. (M)ethode: 4 voisins ou 8 voisins\n");
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("3. Changer (E)psilon");
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("4. Changer (C)oefficient");
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("5. (V)ariation de temperature\n");
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("6. (F)ixer temperature\n");
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("7. (S)imulation\n");
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("8. (Q)uitter le programme\n");
-
-    gotoxy(COL_DROITE, ++ligne_ecriture);
-    printf("Choisissez une option: ");
-    choix = toupper(_getch());
-
-    switch (choix) {
-    case 'L': case '1': {
-        FILE* fichier = lire_nom_fich();
-        if (fichier) {
-            lire_fichier(fichier, plaque, pos_fixes, dimy, dimx, mint, maxt);
-            *init = 1;
-        }
-        colorage_plaque(plaque, *dimy, *dimx, *mint, *maxt);
-
-        break;
-    }
-    case 'M': case '2':
-        do {
-            printf("Choisissez la methode (4 ou 8): ");
-            scanf("%d", mode);
-        } while (*mode != 4 && *mode != 8);
-        break;
-    case 'E': case '3':
-        do {
-            printf("Entrez une valeur pour epsilon (0.01 - 10.0): ");
-            scanf("%lf", epsilon);
-        } while (*epsilon < 0.01 || *epsilon > 10.0);
-        break;
-    case 'C': case '4':
-        do {
-            printf("Entrez une valeur pour le coefficient (0.20 - 0.80): ");
-            scanf("%lf", coeff);
-        } while (*coeff < 0.20 || *coeff > 0.80);
-        break;
-    case 'V': case '5':
-        if (*init) {
-            printf("Fonction de variation de temperature à implementer.\n");
-        }
-        else {
-            printf("Veuillez d'abord charger les donnees.\n");
-        }
-        break;
-    case 'F': case '6':
-        if (*init) {
-            printf("Fonction de fixation de la temperature à implementer.\n");
-        }
-        else {
-            printf("Veuillez d'abord charger les donnees.\n");
-        }
-        break;
-    case 'S': case '7':
-        if (*init) {
-            iteration_n = simulation(plaque, pos_fixes, *dimy,
-                *dimx, mint, maxt, *mode, *epsilon, *coeff);
-            gotoxy(COL_DROITE, LINGNE_INFO_TABLE + 1);
-            printf("Simulation terminee apres %d iterations.\n", iteration_n);
-        }
-        else {
-            printf("Veuillez d'abord charger les données.\n");
-        }
-        break;
-    case 'Q': case '8':
-        printf("Quitter le programme.\n");
-        return 0;
-    default:
-        printf("Choix invalide. Essayez à nouveau.\n");
-        break;
-    }
-
-    return 1;
-}
-
 
 void affichage_plaque(const t_matrice plaque, int dimy, int dimx, double mint, double maxt) {
     int i, j, n_couleur;
@@ -267,17 +143,14 @@ void aff_options(int mode, double epsi, double coeff_res) {
     int ligne_affichage = 15;  // Ligne située sous le menu principal
     int col_affichage = COL_DROITE; // Position à droite de l’écran
 
-    gotoxy(col_affichage, ligne_affichage);
-    printf("Options actuelles:");
-
     gotoxy(col_affichage, ligne_affichage + 1);
-    printf("Mode : %d voisins", mode);
+    printf("mode : %d voisins", mode);
 
     gotoxy(col_affichage, ligne_affichage + 2);
-    printf("Epsilon : %.2lf", epsi);
+    printf("epsilon : %.2lf", epsi);
 
     gotoxy(col_affichage, ligne_affichage + 3);
-    printf("Coefficient : %.2lf", coeff_res);
+    printf("coefficient : %.2lf", coeff_res);
 
 }
 
@@ -301,11 +174,16 @@ int valider_mode_voisins() {
         gotoxy(col_affichage, ligne_affichage);
         printf("Mode de calcul (4 ou 8 voisins) : ");
         scanf("%d", &mode);
+        gotoxy(col_affichage, ligne_affichage);
+        printf("                                            "); // Effacer la ligne d'erreur
 
         if (mode != 4 && mode != 8) {
             gotoxy(col_affichage, ligne_affichage + 1);
             printf("Erreur: entrez 4 ou 8 uniquement.");
+            FFLUSH(); 
         }
+        gotoxy(col_affichage, ligne_affichage);
+        printf("                                ");
     } while (mode != 4 && mode != 8);
 
     // Efface le message d'erreur après saisie correcte
@@ -325,6 +203,9 @@ double valider_reel(const char* message, double b_min, double b_max) {
         gotoxy(col_affichage, ligne_affichage);
         printf("%s (entre %.2lf et %.2lf) : ", message, b_min, b_max);
         scanf("%lf", &valeur);
+        FFLUSH(); 
+        gotoxy(col_affichage, ligne_affichage); // Efface
+        printf("                                                                   "); 
 
         if (valeur < b_min || valeur > b_max) {
             gotoxy(col_affichage, ligne_affichage + 1);
@@ -334,7 +215,42 @@ double valider_reel(const char* message, double b_min, double b_max) {
 
     // Efface le message d'erreur après saisie correcte
     gotoxy(col_affichage, ligne_affichage + 1);
-    printf("                                    ");  // Efface la ligne d'erreur
+    printf("                                             ");  // Efface la ligne d'erreur
+
+   
 
     return valeur;
+}
+
+unsigned char afficher_menu() {
+
+    const char* message = "Nouvelle valeur de epsilon?: ";
+    int ligne_ecriture = 0;  // Position du menu en bas de l'écran
+    unsigned char choix;
+
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("Menu:\n");
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("1. (L)ire donnees initiales d'un fichier\n");
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("2. (M)ethode: 4 voisins ou 8 voisins\n");
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("3. Changer (E)psilon");
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("4. Changer (C)oefficient");
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("5. (V)ariation de temperature\n");
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("6. (F)ixer temperature\n");
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("7. (S)imulation\n");
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("8. (Q)uitter le programme\n");
+
+    gotoxy(COL_DROITE, ++ligne_ecriture);
+    printf("Choisissez une option: ");
+
+    choix = toupper(_getch());
+
+    return choix;
 }
